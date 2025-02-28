0c2e71db48d6885a8e159f65e5cea2f25bf6511a
Angel <22824872+Zyplos@users.noreply.github.com>
2025-02-28 16:19:34 -0600

    intro header some styling adjustments

--- src/components/IntroHeader/index.tsx
+++ src/components/IntroHeader/index.tsx
@@ -6,8 +6,13 @@ export default function IntroHeader() {
 		<div className="intro-header">
 			<div className="copy-area">
 				<h2 className="display-text">Now you're playing with power.</h2>
-				<p className="price">From $599</p>
-				<p className="financing">or $49.91/month with 12-month financing*</p>
+				<div>
+					<p className="price">From $599</p>
+					<p className="financing">or $49.91/month with 12-month financing*</p>
+				</div>
+				<div>
+					<button type="button">Buy</button>
+				</div>
 			</div>
 			<div className="product-image">
 				<img src={frontView} alt="Pixel Base Front View" />
