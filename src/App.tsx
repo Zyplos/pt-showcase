797d9a8062b6d91464f337d6d88f09b8e658027b
Angel <22824872+Zyplos@users.noreply.github.com>
2025-02-28 10:33:12 -0600

    page nav header done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
+import PageNavHeader from "./components/PageNavHeader";
 import "./index.css";
 
 export function App() {
 	return (
-		<div className="root">
-			<div className="page-nav-header">
-				<h1 className="product-title">Pixel Base</h1>
-				<div className="nav-items">
-					<p className="active-link-fake">Overview</p>
-					<button type="button">Toggle Design Commentary</button>
-					<div className="buy-box">
-						<div>
-							<p>From $599</p>
-							<p>or $49.91/month with 12-month financing*</p>
-						</div>
-						<button type="button">Buy</button>
-					</div>
-				</div>
-			</div>
+		<>
+			<section className="limited-width">
+				<PageNavHeader />
+			</section>
 
 			<div className="intro-header">pow!</div>
-		</div>
+		</>
 	);
 }
 
---
+++ src/components/PageNavHeader/index.tsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function PageNavHeader() {
+	return (
+		<div className="page-nav-header">
+			<h1 className="product-title">Pixel Base</h1>
+
+			<div className="nav-items">
+				<p className="active-link">
+					<span>Overview</span>
+				</p>
+				<button type="button">Toggle Design Commentary</button>
+			</div>
+
+			<div className="buy-box">
+				<div>
+					<p className="price">From $599</p>
+					<p className="financing">or $49.91/month with 12-month financing*</p>
+				</div>
+				<button type="button">Buy</button>
+			</div>
+		</div>
+	);
+}
